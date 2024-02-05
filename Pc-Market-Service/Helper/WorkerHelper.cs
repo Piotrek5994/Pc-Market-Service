b409ede2d52ea816271dc86983ffbf73f021261a
PiotrekD5994 <138677143+PiotrekD5994@users.noreply.github.com>
2024-02-05 13:14:27 +0100

    Add interval for worker

---
+++ Pc-Market-Service/Helper/WorkerHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pc_Market_Service.Helper
+{
+    public class WorkerHelper
+    {
+        private Timer _timer;
+
+        public WorkerHelper()
+        {
+            ScheduleNextRun();
+        }
+
+        private void ScheduleNextRun()
+        {
+            var now = DateTime.Now;
+            var nextRun = now.Date.AddHours(8); // Następne uruchomienie dzisiaj o 8 rano
+            if (now > nextRun)
+            {
+                // Jeśli już po 8, planujemy na jutro
+                nextRun = nextRun.AddDays(1);
+            }
+
+            var dueTime = nextRun - now; // Obliczamy, ile czasu pozostało do uruchomienia
+            _timer = new Timer(DoWork, null, dueTime, Timeout.InfiniteTimeSpan); // Ustawiamy timer na jednorazowe uruchomienie
+        }
+
+        private void DoWork(object state)
+        {
+            // Logika, która ma być wykonana o 8 rano
+            Console.WriteLine("Wykonywanie pracy: " + DateTime.Now.ToString());
+
+            // Po wykonaniu zadania, planujemy następne uruchomienie
+            ScheduleNextRun();
+        }
+    }
+}
