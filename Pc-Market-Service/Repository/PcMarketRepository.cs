4c7311ded0baf698f2b910ff349580c01dcd7f23
Piotrek5994 <138677143+Piotrek5994@users.noreply.github.com>
2024-02-07 12:04:20 +0100

    Add method withone read document in Pc-Market

--- Pc-Market-Service/Repository/PcMarketRepository.cs
+++ Pc-Market-Service/Repository/PcMarketRepository.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
 using Pc_Market_Service.Configuration;
 using Pc_Market_Service.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +20,33 @@ namespace Pc_Market_Service.Repository
         {
             _sqlConfiguration = sqlConfiguration.Value;
             _log = log;
+        }
+        public DataTable GetDocumentList()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_sqlConfiguration.ConnectionString))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM Documents";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                        {
+                            da.Fill(dt);
+                        }
+                    }
+                    _log.LogInformation("Successfully got document data");
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.LogError("Failed to connect to the database");
+            }
+
+            return dt;
         }
     }
 }
