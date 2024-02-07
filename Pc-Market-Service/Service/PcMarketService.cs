caf7acd8e0e79ba0fd6a86ea000248a2054e2e7e
Piotrek5994 <138677143+Piotrek5994@users.noreply.github.com>
2024-02-07 12:20:18 +0100

    Add independancy injaction

--- Pc-Market-Service/Service/PcMarketService.cs
+++ Pc-Market-Service/Service/PcMarketService.cs
@@ -1,4 +1,5 @@
-﻿using Pc_Market_Service.Service.IService;
+﻿using Pc_Market_Service.Repository.IRepository;
+using Pc_Market_Service.Service.IService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,11 @@ namespace Pc_Market_Service.Service
 {
     public class PcMarketService : IPcMarketService
     {
+        private readonly IPcMarketRepository _repository;
+
+        public PcMarketService(IPcMarketRepository repository)
+        {
+            _repository = repository;
+        }
     }
 }
--- Pc-Market-Service/Worker/PcMarketWorker.cs
+++ Pc-Market-Service/Worker/PcMarketWorker.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Pc_Market_Service.Service.IService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,13 @@ namespace Pc_Market_Service.Worker
 {
     public class PcMarketWorker : BackgroundService
     {
+        private readonly IPcMarketService _service;
+
+        public PcMarketWorker(IPcMarketService service)
+        {
+            _service = service;
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             throw new NotImplementedException();
